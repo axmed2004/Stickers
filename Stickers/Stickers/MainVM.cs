29a7bcf94bfe7d1fbec8b857691756e4c430e384
axmed2004 <axmed2004@mail.ru>
2022-10-28 18:30:45 +0300

    Add files via upload

---
+++ Stickers/Stickers/MainVM.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Stickers
+{
+    class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string docText { get; set; }
+        public int fontSize { get; set; }
+        
+
+        public string DocText
+        {
+            get { return docText; }
+            set
+            {
+                docText = value;
+                OnPropertyChanged("DocText");
+            }
+        }
+
+        public int FontSize
+        {
+            get { return fontSize; }
+            set
+            {
+                fontSize = value;
+                OnPropertyChanged("FontSize");
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        Cmd openFileCmd;
+        Cmd windowLoaded;
+        Cmd setFontSize;
+        Cmd incrFontSize;
+        Cmd decrFontSize;
+
+        public Cmd OpenFileCmd
+        {
+            get
+            {
+                return openFileCmd ?? (openFileCmd = new Cmd(obj =>
+                {
+                    OpenFileDialog opf = new OpenFileDialog
+                    {
+                        DefaultExt = ".mp3",
+                        Multiselect = false,
+                        Filter = "Текст|*.txt;"
+                    };
+                    bool? result = opf.ShowDialog();
+                    if (result == true)
+                    {
+                        Properties.Settings.Default.lastFile = opf.FileName;
+                        Properties.Settings.Default.Save();
+                        DocText = File.ReadAllText(opf.FileName);
+                    }
+                }));
+            }
+        }
+
+        public Cmd WindowLoaded
+        {
+            get
+            {
+                return windowLoaded ?? (windowLoaded = new Cmd(obj =>
+                {
+                    if (Properties.Settings.Default.lastFile != "")
+                    {
+                        string f = Properties.Settings.Default.lastFile;
+                        if (File.Exists(f))
+                        {
+                            DocText = File.ReadAllText(f);
+                        }
+                    }
+                }));
+            }
+        }
+
+        public Cmd SetFontSize
+        {
+            get
+            {
+                return windowLoaded ?? (windowLoaded = new Cmd(obj =>
+                {
+                    
+                }));
+            }
+        }
+
+        public Cmd IncrFontSize
+        {
+            get
+            {
+                return incrFontSize ?? (incrFontSize = new Cmd(obj =>
+                {
+                    FontSize++;
+                    Properties.Settings.Default.fontSize = FontSize;
+                    Properties.Settings.Default.Save();
+                }));
+            }
+        }
+
+        public Cmd DecrFontSize
+        {
+            get
+            {
+                return decrFontSize ?? (decrFontSize = new Cmd(obj =>
+                {
+                    if(FontSize>1)
+                        FontSize--;
+                    Properties.Settings.Default.fontSize = FontSize;
+                    Properties.Settings.Default.Save();
+                }));
+            }
+        }
+
+        public MainVM()
+        {
+            if (Properties.Settings.Default.lastFile != "")
+            {
+                FontSize = Properties.Settings.Default.fontSize;
+                string f = Properties.Settings.Default.lastFile;
+                if (File.Exists(f))
+                {
+                    DocText = File.ReadAllText(f);
+                }
+            }
+        }
+
+    }
+}
---
+++ Stickers/Stickers/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stickers
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int GWL_HWNDPARENT = -8;
+        #region DLL
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpWindowClass, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainVM();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            IntPtr hprog = FindWindowEx(
+                FindWindowEx(
+                    FindWindow("Progman", "Program Manager"),
+                    IntPtr.Zero, "SHELLDLL_DefView", ""),
+                IntPtr.Zero, "SysListView32", "FolderView"
+             );
+            //prevent hide on show desktop
+            SetWindowLong(new WindowInteropHelper(this).Handle, GWL_HWNDPARENT, hprog);
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void CloseWindow_button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
